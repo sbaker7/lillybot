fc8466ed454dedb66f2d4518b0479d13294652ac
Stephanie Baker <sljulia.baker@gmail.com>
2017-02-17 13:51:52 +1100

    NEW: started the implementation of single line commands

--- plugins/spambot/__plugin__.rb
+++ plugins/spambot/__plugin__.rb
@@ -16,9 +16,9 @@ Lilly.plugin.define 'Spambot' do
     puts "SPAMBOT DEBUG: turning on"
     message = "Go Astrious, go! Make sure to follow Astrious on twitter, @Astriousruns. You can play games and talk to me too! Try !commands"
 
-    # @job = @scheduler.every '30s', first_in: '0s' do
-    #   send_message message
-    # end
+    @job = @scheduler.every '30s', first_in: '0s' do
+      puts message
+    end
 
     "This plugin doesn't work yet. It will be ready soon"
   end
