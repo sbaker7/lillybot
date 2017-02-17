2ce1f79d24570ea7b7c5e6ff3685c85093c8636d
Stephanie Baker <sljulia.baker@gmail.com>
2017-02-17 11:45:42 +1100

    NEW: added !chance command

---
+++ plugins/chance/README.md
@@ -0,0 +1,7 @@
+# Chance
+
+The following commands are accepted:
+
+| Command | Function |
+| ------- | -------- |
+|!chance | Play chance with Lilly! |
---
+++ plugins/chance/__plugin__.rb
@@ -0,0 +1,25 @@
+Lilly.plugin.define 'Slots' do
+
+  on(:system_start) do
+    path = File.expand_path('../res/faces.json', __FILE__)
+    file = File.read(path)
+    @faces = JSON.parse(file)
+  end
+
+  on(:system_stop) do
+    # do nothing?
+  end
+
+  on(:chance) do |user, message|
+
+    result = rand(5) + 1
+    responses = ["You rolled: #{result}"]
+    if result == 3
+        responses << "Bad luck, #{user}! You've been timed out for 20 seconds"
+        responses << ".timeout #{user} 20"
+    else
+        responses << "What good luck you have, #{user}!"
+    end
+  end
+
+end
