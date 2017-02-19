ade4d6c4e9fae6cc986bfa584f2ccb129eba61fd
Stephanie Baker <sljulia.baker@gmail.com>
2017-02-19 20:47:01 +1100

    FIX: more issues with quotes not being sent properly

--- plugins/commands/__plugin__.rb
+++ plugins/commands/__plugin__.rb
@@ -53,6 +53,7 @@ Lilly.plugin.define 'Commands' do
   on(:raw_message) do |user, message|
     if valid_key = @commands.keys.select { |key| message.to_s.match(Regexp.new(key, true)) }.first
       begin
+        puts @commands[valid_key]
         if @commands[valid_key].kind_of?(Array)
           @commands[valid_key].each { |c| eval("\"#{c}\"")}
         else
